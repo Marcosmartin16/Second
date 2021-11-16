bcc5861d2314d15cb7a04dadf58bf29c98f91af1
marcosmartin16 <sr_eme@outlook.es>
2021-11-16 09:44:06 +0100

    app bar

--- app/src/main/java/com/first/second/Main.java
+++ app/src/main/java/com/first/second/Main.java
@@ -1,8 +1,21 @@
 package com.first.second;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.lang.invoke.ConstantCallSite;
 
 public class Main extends AppCompatActivity {
 
@@ -10,5 +23,61 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView mycontext = (TextView) findViewById(R.id.textTap);
+        registerForContextMenu(mycontext);
     }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.menu_context,menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+//                Toast toast = Toast.makeText(this, "Item copied",
+//                        Toast.LENGTH_LONG);
+//                toast.show();
+
+              final  ConstraintLayout mLayout =  findViewById(R.id.myMainConstraint);
+
+                Snackbar snackbar = Snackbar
+                        .make(mLayout, "aaaaaaaaaaaaaaaaaa", Snackbar.LENGTH_LONG)
+                        .setAction("UNDu", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Snackbar snackbar1 = Snackbar.make(mLayout, "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb", Snackbar.LENGTH_SHORT);
+                                snackbar1.show();
+                            }
+                        });
+
+                snackbar.show();
+
+
+                return true;
+
+            case R.id.item2:
+                Toast toast2 =
+                        Toast.makeText(this, "Item download", Toast.LENGTH_SHORT);
+                toast2.show();
+                return true;
+
+            default:
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.app_bar,menu);
+        return true;
+    }
+
+    //public void openRegister(View v){
+    //  Intent intent = new Intent(Main.this, Register.class);
+    //        startActivity(intent);
+    //  }
 }
